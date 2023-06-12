bf2b9f9ef0fd120124c3504797929b79ed283a25
Logeshwaran <logeshsiva395@gmail.com>
2023-06-12 16:44:41 +0530

    final

--- src/App/Config/servAddr.js
+++ src/App/Config/servAddr.js
@@ -1,6 +1,7 @@
 const path = {
-    local: 'https://cartify-backend-production.up.railway.app',
+    // local: 'https://cartify-backend-production.up.railway.app',
     // local : 'http://localhost:4000',
+    local: 'https://cartify-backend.onrender.com',
     remote: 'https://cartify-backend.herokuapp.com',
 }
 
